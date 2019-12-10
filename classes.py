9d981ca2af79c634f0aca64815082dde2ddd6c05
Conrad Bohm <CBohm@spartaglobal.com>
2019-12-10 11:31:26 +0000

    set out base class structure plus some methods

---
+++ classes.py
@@ -0,0 +1,32 @@
+class Passengers:
+    def __init__(self, name, pass_num):
+        self.name = name
+        self.passport_number = pass_num
+
+    def new_passenger(self, name, pass_num):
+        pass
+
+
+class Plane:
+    def __init__(self, number):
+        self.id_number = number
+
+
+class Flight:
+    def __init__(self):
+        self.plane = ''
+        self.destination = ''
+        self.origin = ''
+        self.passengers = []
+
+    def add_plane(self, plane):
+        pass
+
+    def add_destination(self, dest):
+        self.destination = dest
+
+    def add_origin(self, origin):
+        self.origin = origin
+
+    def add_passenger(self, passenger):
+        self.passengers.append(passenger)
