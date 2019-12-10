800d104644d53def806f7d9ad4bfa607f57d552e
Conrad Bohm <CBohm@spartaglobal.com>
2019-12-10 12:04:55 +0000

    added and passed plane creation test

--- classes_functions_test.py
+++ classes_functions_test.py
@@ -9,3 +9,11 @@ def test_user_creation():
     new_pass = new_passenger('Birt Kuman', 'B13927')
     assert new_pass.name == 'Birt Kuman'
     assert new_pass.passport_number == 'B13927'
+
+
+def test_plane_creation():
+    test_plane = Plane('0178945')
+    assert test_plane.id_number == '0178945'
+
+# def test_flight_add_plane():
+#     test_flight = Flight()
