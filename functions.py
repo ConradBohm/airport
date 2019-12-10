34a87ee8c5771171924bdbb1ef6a6ef39d3b87d0
Conrad Bohm <CBohm@spartaglobal.com>
2019-12-10 14:01:07 +0000

    added make passenger functionality, with non-value check; menu structure; passenger list print

--- functions.py
+++ functions.py
@@ -1,7 +1,47 @@
 from classes import *
-passenger_list = []
 
 
 def new_passenger(name, number):
     new_pass = Passenger(name, number)
     return new_pass
+
+
+def new_pass_input_validation(input_str):
+    if not input_str.strip():
+        print('You cannot leave this field blank.')
+        return False
+    else:
+        return True
+
+
+def pass_add():
+    while True:
+        name = input('Enter a name: ')
+        if not new_pass_input_validation(name):
+            continue
+        else:
+            break
+
+    while True:
+        number = input('Enter a passport number: ')
+        if not new_pass_input_validation(number):
+            continue
+        else:
+            break
+
+    new_pass = new_passenger(name, number)
+    passenger_list.append(new_pass)
+
+
+def menu_input():
+    while True:
+        try:
+            choice = int(input('Pick a number from the menu options: '))
+            break
+        except ValueError:
+            print('\nNot a valid number, try again with a number digit from the menu.')
+
+    return choice
+
+
+
--- main.py
+++ main.py
@@ -1,2 +1,35 @@
-import classes
+from functions import *
+passenger_list = []
+name_list = []
+plane_list = []
+
+print('----///---- Airport Program ----\\\\\\----\n\n')
+
+while True:
+    print('Menu:\n'
+          '1 -- Add Passenger\n'
+          '2 -- List Passengers\n'
+          '3 -- Add Plane\n'
+          '4 -- Flights\n'
+          '5 -- Exit')
+
+    choice = menu_input()
+
+    if choice == 1:
+        pass_add()
+
+    elif choice == 2:
+        for person in passenger_list:
+            name_list.append(person.name)
+            print(name_list)
+
+    elif choice == 3:
+        pass
+
+    elif choice == 4:
+        print('Thanks for using the program.')
+        break
+    else:
+        print('We don\'t have that option. Try again.')
+
 
